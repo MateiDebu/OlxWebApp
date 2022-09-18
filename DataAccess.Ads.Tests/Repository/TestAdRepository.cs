8929e61cab9a330a6beb5d9bf3adcfe014c9b847
MateiDebu <102983526+MateiDebu@users.noreply.github.com>
2022-09-18 21:57:03 +0300

    Added some tests for UserRepository and AdsRepository.

---
+++ DataAccess.Ads.Tests/Repository/TestAdRepository.cs
@@ -0,0 +1,74 @@
+﻿using DataAccess.Announcement.Repository;
+using DataAccess.Database.Context;
+using DataAccess.Database.Models;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace DataAccess.Ads.Tests.Repository
+{
+    [TestFixture]
+    public class TestAdRepository
+    {
+        
+        private AdsRepository _adsRepository;
+        private OlxContext _olxContext;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            var builder = new DbContextOptionsBuilder<OlxContext>().UseInMemoryDatabase("TestUserRepository" +
+                TestContext.CurrentContext.Test.Name);
+            _olxContext = new OlxContext(builder.Options);
+            await _olxContext.Database.EnsureCreatedAsync();
+
+            _adsRepository = new AdsRepository(_olxContext);
+        }
+
+        [Test]
+
+        public async Task AdsRepository_CreateAd_ValidParameters_ReturnAd()
+        {
+            //Arrange
+            var appUser = new ApplicationUser() { Name = "name", UserName = "username" };
+            _olxContext.Users.Add(appUser);
+            await _olxContext.SaveChangesAsync();
+
+            //Act
+            var result = await _adsRepository.CreateAd(appUser.Id, "name", "description");
+
+            //Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.Not.Zero);
+            Assert.That(result.Name, Is.EqualTo("name"));
+            Assert.That(result.Description, Is.EqualTo("description"));
+        }
+
+        [Test]
+        public async Task AdsRepository_GetAllAdsForUser_ValidParameters_RetrunsAds()
+        {
+            //Arrange
+            var appUser = new ApplicationUser() { Name = "name", UserName = "username" };
+            _olxContext.Users.Add(appUser);
+
+            var ad1=new Ad() { UserId=appUser.Id,Name="name1",Description="description1"};
+            var ad2 = new Ad() { UserId = appUser.Id, Name = "name2", Description = "description2" };
+            _olxContext.Announcements.Add(ad1);  
+            _olxContext.Announcements.Add(ad2);  
+
+            await _olxContext.SaveChangesAsync();
+
+            //Act
+            var result = _adsRepository.GetAllAdsForUser(appUser.Id, 0, 10);
+            var resultList = result.ToList();
+
+            //Assert 
+            Assert.That(result, Is.Not.Null);
+            Assert.That(resultList , Has.Count.EqualTo(2));
+            Assert.That(resultList[0].Name, Is.EqualTo("name1"));
+            Assert.That(resultList[0].Description, Is.EqualTo("description1"));
+            Assert.That(resultList[1].Name, Is.EqualTo("name2"));
+            Assert.That(resultList[1].Description, Is.EqualTo("description2"));
+
+        }
+    }
+}
---
+++ DataAccess.Users.Tests/Repository/TestUserRepository.cs
@@ -0,0 +1,97 @@
+﻿using DataAccess.Database.Context;
+using DataAccess.Database.Models;
+using DataAccess.Users.Repository;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Frameworks;
+using NUnit.Framework;
+
+namespace DataAccess.Users.Tests.Repository
+{
+    [TestFixture]
+    public class TestUserRepository
+    {
+        private UserRepository _userRepository;
+        private OlxContext _olxContext;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            var builder = new DbContextOptionsBuilder<OlxContext>().UseInMemoryDatabase("TestUserRepository"+
+                TestContext.CurrentContext.Test.Name);
+            _olxContext = new OlxContext(builder.Options);
+            await  _olxContext.Database.EnsureCreatedAsync();
+
+            _userRepository = new UserRepository(_olxContext);
+        }
+
+        [Test]
+        public async Task UserRepository_Add_ValidUser_UserIsAdded()
+        {
+            //Arrange
+            string name = "name";
+            string username = "username";
+            var appUser = new ApplicationUser() { Name = name, UserName = username };
+
+            //Act
+            var addedUser = await _userRepository.Add(appUser);
+
+            //Assert
+            Assert.That(addedUser, Is.Not.Null);
+            Assert.That(addedUser.Id, Is.Not.Zero);
+            var dbUser = _olxContext.Users.First();
+            Assert.That(addedUser,Is.SameAs(dbUser));
+        }
+
+        [Test]
+        public async Task UserRepository_FindById_UserFound_ReturnUser()
+        {
+            //Arrange
+            string name = "name";
+            string username = "username";
+            var appUser = new ApplicationUser() { Name = name, UserName = username };
+            _olxContext.Users.Add(appUser);
+            await _olxContext.SaveChangesAsync();
+
+            //Act
+            var result = await _userRepository.FindById(appUser.Id);
+
+            //Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result,Is.SameAs(appUser));
+        }
+
+        [Test]
+        public async Task UserRepository_FindById_UserNotFound_ReturnNull()
+        {
+            //Act
+            var result = await _userRepository.FindById(101);
+
+            //Assert
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public async Task UserRepository_Delete_UserFound_UserIsDeleted()
+        {
+            //Arrange
+            string name = "name";
+            string username = "username";
+            var appUser = new ApplicationUser() { Name = name, UserName = username };
+            _olxContext.Users.Add(appUser);
+            await _olxContext.SaveChangesAsync();
+
+            //Act
+            await _userRepository.Delete(appUser.Id);
+
+            //Assert
+            Assert.That(_olxContext.Users, Is.Empty);
+        }
+
+        [Test]
+        public void UserRepository_Delete_UserNotFound_DoesNothing()
+        {
+            //Act & assert
+            Assert.That(()=>_userRepository.Delete(101), Throws.Nothing);
+        }
+    }
+}
